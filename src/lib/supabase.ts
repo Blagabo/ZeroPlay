8df5596436b1d746b0ab56c331d48f342cf15a22
Blagabo <gabrielblanconoce5@gmail.com>
2024-11-29 18:48:10 +0100

    test env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js"
 
 export const supabase = createClient(
 	"https://lmdqbgkvyanzprplgoxl.supabase.co",
-	import.meta.env.SUPABASE_ANON_KEY
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtZHFiZ2t2eWFuenBycGxnb3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4MzMyNTksImV4cCI6MjA0ODQwOTI1OX0.gBC-qsUzJ7y_f-5RAzjYZCDuOTkROKQonBk6IgtxFg8"
 )
