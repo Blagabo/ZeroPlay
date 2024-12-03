df96fed2061703f5cf17e01354f3e1bcbb67ae3b
Blagabo <gabrielblanconoce5@gmail.com>
2024-12-03 18:10:53 +0100

    fix middle

--- src/middleware.ts
+++
@@ -1,5 +0,0 @@
-import { sequence } from "astro:middleware"
-import { handleAdminRedirect } from "./middleware/admin"
-import { handleAuth } from "./middleware/auth"
-
-export const onRequest = sequence(handleAuth, handleAdminRedirect)
--- src/middleware/admin.ts
+++
@@ -1,9 +0,0 @@
-import type { MiddlewareHandler } from "astro"
-
-export const handleAdminRedirect: MiddlewareHandler = async ({ url, redirect }, next) => {
-	if (url.pathname === "/admin") {
-		return redirect("/admin/dashboard")
-	}
-
-	return next()
-}
